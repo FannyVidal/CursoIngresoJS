12061bab835371987c87d788d16beca3f266a8df
FannyVidal <52494958+FannyVidal@users.noreply.github.com>
2019-07-11 12:32:06 -0300

    Add files via upload
    tratar de hacer los ejercicios anteriores

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,24 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+	var numero;
+	var contador = 0;
+	// aka suma 
+	var acumulador = 0;
+	var promedio;
 
+	while (contador < 5 ){
+		//hay que tomar en cuenta que dentro del repetidor se debe poner lo que se repita en este caso serian
+		//los numeros que se piden y las veces que se suma
+		numero = parseInt(prompt("Ingrese número: "));
+		acumulador = acumulador + numero ;
+		//el ++ significa contador + 1
+		contador++;
+	}
 
+	promedio = acumulador / 5;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
