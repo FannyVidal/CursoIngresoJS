5de73519ed59dc972c0ef01a4b50772664563d12
FannyVidal <52494958+FannyVidal@users.noreply.github.com>
2019-07-18 00:32:46 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,10 +1,23 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	var numero;
+	var contador = 0;
+	var acumulador = 0;
+	var promedio;
+	var respuesta = 'si';
 
+	do{
+
+		numero = parseInt(prompt("Ingrese número"));
+		acumulador = acumulador + numero;
+		contador++;
+
+		numero = prompt("¿Quiere ingresar otro número?");
+
+	} while(respuesta == 'si');
+
+	
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
